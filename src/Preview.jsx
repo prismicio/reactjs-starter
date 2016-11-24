4c7f3ae34a71aaa02f59e53b06c7fddb1a7174ab
Sebastien Renault <srenault.contact@gmail.com>
2016-11-24 22:49:48 +0100

    Use js-cookie to set preview cookie

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { browserHistory } from 'react-router';
 import PrismicToolbar from 'prismic-toolbar';
+import Cookies from 'js-cookie';
 import Prismic from 'prismic.io';
 import Config from '../prismic-configuration';
 
@@ -9,14 +10,10 @@ const PREVIEW_EXPIRES = 30 * 60 * 1000; // 30 minutes
 
 export default class Preview extends React.Component {
 
-  static setRef(token) {
-    document.cookie = `${Prismic.previewCookie}=${token}; expires=${PREVIEW_EXPIRES}`;
-  }
-
   componentDidMount() {
     const token = this.props.location.query.token;
     const url = this.props.ctx.api.previewSession(token, Config.linkResolver, '/');
-    Preview.setRef(token);
+    Cookies.set(Prismic.previewCookie, token, { expires: PREVIEW_EXPIRES });
     PrismicToolbar.toolbar();
     browserHistory.push(url);
     return url;
