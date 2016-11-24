d197bc4252bfe9c0988b2fe98fe3a4f53830c0c8
Sebastien Renault <srenault.contact@gmail.com>
2016-11-24 23:25:41 +0100

    Upgrade dependencies and add badge

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loaders: ['react-hot', 'babel']
+        loaders: ['babel']
       },
       {
         test: /\.json$/,
