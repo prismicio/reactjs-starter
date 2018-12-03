cc811e28089bc34be1067918d0c65908d5b30c4d
Levi Gable <levimykel@gmail.com>
2018-12-03 15:55:19 +0100

    Preview cookie loaded by the toolbar

--- src/Preview.js
+++ src/Preview.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
-import Cookies from 'js-cookie';
 import qs from 'qs';
-import Prismic from 'prismic-javascript';
 import PrismicConfig from './prismic-configuration';
-
-const PREVIEW_EXPIRES = 1/48; // 30 minutes
 
 export default class Preview extends React.Component {
 
   componentWillReceiveProps(props) {
     const params = qs.parse(props.location.search.slice(1));
     props.prismicCtx.api.previewSession(params.token, PrismicConfig.linkResolver, '/').then((url) => {
-      Cookies.set(Prismic.previewCookie, params.token, { expires: PREVIEW_EXPIRES });
       props.history.push(url);
     });
   }
