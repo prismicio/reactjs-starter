bb0d7bc685f76c8d76603a9b9d662529f094cf1d
Levi Gable <levimykel@gmail.com>
2019-11-22 14:57:16 +0100

    Update and document the prismic-configuration

--- src/prismic-configuration.js
+++ src/prismic-configuration.js
@@ -1,21 +1,19 @@
 import Prismic from 'prismic-javascript'
 
+// -- Prismic API endpoint
+// Determines which repository to query and fetch data from
 export const apiEndpoint = 'https://your-repo-name.cdn.prismic.io/api/v2'
 
-// -- Access token if the Prismic repository is not public
+// -- Access Token if the repository is not public
+// Generate a token in your dashboard and configure it here if your repository is private
 const accessToken = ''
 
-// OAuth
-// clientId: 'xxxxxx',
-// clientSecret: 'xxxxxx',
-
-// -- Links resolution rules
-// This function will be used to generate links to Prismic documents
-// As your project grows, you should update this function according to your routes
-
+// -- Link resolution rules
+// Manages the url links to internal Prismic documents
 export const linkResolver = (doc) => {
   if (doc.type === 'page') return `/page/${doc.uid}`
   return '/'
 }
 
+// Client method to query documents from the Prismic repo
 export const client = Prismic.client(apiEndpoint, { accessToken })
